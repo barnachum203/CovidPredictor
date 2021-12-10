7948d24f7fd25b6d123f99efe4d926fbb69fbe18
sagileyba <49640454+sagileyba@users.noreply.github.com>
2021-12-10 18:34:00 +0200

    gateway - creating spring security
    creating spring security with user authentication.
    creating web page
    testing for user accessing

--- gateway/src/main/java/com/philips/project/gateway/controllers/APIGateway.java
+++ gateway/src/main/java/com/philips/project/gateway/controllers/APIGateway.java
@@ -1,15 +1,25 @@
 package com.philips.project.gateway.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
-@RequestMapping("project")
+import com.philips.project.gateway.bean.User;
+import com.philips.project.gateway.repositories.UserRepository;
+import com.philips.project.gateway.service.CustomUserDetailsService;
+
+//@RestController
+@Controller
+@RequestMapping("/")
 public class APIGateway {
     private static String updatedb_URL = "http://localhost:8082/person/updateResult";
-    private static String report_URL = "http://localhost:8081/report/";
+    private static String report_URL = "http://localhost:8081/report/predict/";
 
     @Autowired
     private RestTemplate client;
@@ -17,9 +27,54 @@ public class APIGateway {
     @GetMapping("/{date}")
     public String getReport(@PathVariable String date)
     {
+    	Thread thread = new Thread();
+    	thread.run();
         System.out.println(date);
+        System.out.println(report_URL+date);
         ResponseEntity<String> result =  this.client.getForEntity(report_URL+date, String.class);
-        return result.getBody();
+        System.out.println(result);
+        return "index";
     }
 
+    @Autowired
+    private UserRepository userRepo;
+    
+    @Autowired
+    private CustomUserDetailsService service;
+    
+    @RequestMapping(value = "/")
+    public String viewHomePage() {
+        return "index";
+    }
+    
+    @RequestMapping(value = "/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+         
+        return "signup_form";
+    }
+    
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+    	boolean userExist =service.checkIfExist( user.getEmail() );
+    	
+    	if(userExist == true) {
+    		return "signup_form";
+    	}
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+         
+        userRepo.save(user);
+         
+        return "register_success";
+    }
+    
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+         
+        return "users";
+    }
 }
