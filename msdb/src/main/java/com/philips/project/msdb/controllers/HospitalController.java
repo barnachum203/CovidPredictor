dcfa7236a6ebfbc12bc63354f611f463a3baf0aa
mahde jbae <mahde19951@gmail.com>
2021-12-12 22:29:32 +0200

    update Numofbeds

--- msdb/src/main/java/com/philips/project/msdb/controllers/HospitalController.java
+++ msdb/src/main/java/com/philips/project/msdb/controllers/HospitalController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,6 +49,19 @@ public class HospitalController {
 		}
         return new ResponseEntity<Integer>(1,HttpStatus.OK);
     }
+    
+    @PutMapping("update/patient/result")
+	public ResponseEntity<?> updateNumber_Of_Beds(int hospitalId, int number_Of_Beds)
+	{
+		try {
+			hospitalService.updateNumber_Of_Beds(hospitalId, number_Of_Beds);
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<Exception>(e,HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+	}
+
     
     
 }
